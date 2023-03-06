e8ad4c1e32190b5606f7a48f6a1924e9b2bfd6cf
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-06 21:52:46 +0300

    test: add test for about component

---
+++ src/pages/__test__/About.spec.tsx
@@ -0,0 +1,13 @@
+import { describe, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { About } from "../About";
+
+describe("About page", () => {
+  test("find About component by text", () => {
+    render(<About />);
+    const aboutElement = screen.getByText(/about/i);
+    console.log(aboutElement.textContent);
+    expect(aboutElement).toBeTruthy();
+  });
+});
