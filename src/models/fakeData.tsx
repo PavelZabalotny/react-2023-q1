8b45b1c380b98af0a11481cd7a08456378640f35
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-09 20:01:32 +0300

    feat: create mock data

---
+++ src/models/fakeData.tsx
@@ -0,0 +1,73 @@
+import { IFakeData } from "../types/fakeData.interface";
+
+export const fakeData: IFakeData[] = [
+  {
+    title: "mattis aliquam faucibus",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "55",
+  },
+  {
+    title: "adipiscing commodo elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "245",
+  },
+  {
+    title: "eget nunc lobortis",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "167",
+  },
+  {
+    title: "faucibus pulvinar elementum",
+    description: "",
+    image: "http://placeimg.com/320/240/any",
+    price: "99",
+  },
+  {
+    title: "proin gravida hendrerit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "114",
+  },
+  {
+    title: "ut placerat orci",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "124",
+  },
+  {
+    title: "morbi blandit cursus",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "148",
+  },
+  {
+    title: "aliquet nec ullamcorper",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "368",
+  },
+  {
+    title: "suspendisse interdum consectetur",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "254",
+  },
+  {
+    title: "gravida in fermentum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://placeimg.com/320/240/any",
+    price: "158",
+  },
+];
---
+++ src/types/fakeData.interface.tsx
@@ -0,0 +1,6 @@
+export interface IFakeData {
+  title: string;
+  description: string;
+  image: string;
+  price: string;
+}
