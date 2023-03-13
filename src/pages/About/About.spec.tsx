07053ad9e885a0e8563a16b9c2c9593e615e6696
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-13 10:17:15 +0300

    test: test for About component

--- src/pages/About/About.spec.tsx
+++ src/pages/About/About.spec.tsx
@@ -1,12 +1,10 @@
-import { describe, expect } from "vitest";
+import { expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import About from "./About";
 
-describe("About page", () => {
-  test("find About component by text", () => {
-    render(<About />);
-    const aboutElement = screen.getByText(/about/i);
-    expect(aboutElement).toBeTruthy();
-  });
+test("render About component", () => {
+  render(<About />);
+  const aboutElement = screen.getByTestId("about");
+  expect(aboutElement).toBeDefined();
 });
