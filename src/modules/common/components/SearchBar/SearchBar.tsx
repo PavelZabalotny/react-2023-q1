38f83d8953142155a9dd1fc0070b1409b8377f63
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-14 13:54:29 +0300

    refactor: save input value to LS when the component is unmounting

--- src/modules/common/components/SearchBar/SearchBar.tsx
+++ src/modules/common/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React, { Component, FormEvent } from "react";
 import { getInputFromLocalStorage, setInputToLocalStorage } from "@/shared/utils/localStorage";
 import { LOCALHOST_INPUT_KEY } from "@/shared/constants";
+import styles from "./SearchBar.module.scss";
 
 interface IState {
   value: string;
@@ -12,14 +13,34 @@ class SearchBar extends Component<Record<string, never>, IState> {
   handleInput(e: FormEvent<HTMLInputElement>) {
     const target = e.target as HTMLInputElement;
     this.setState(() => ({ value: target.value }));
-    setInputToLocalStorage(LOCALHOST_INPUT_KEY, target.value);
+  }
+
+  saveToLocalStorage() {
+    setInputToLocalStorage(LOCALHOST_INPUT_KEY, this.state.value);
+  }
+
+  componentDidMount() {
+    window.addEventListener("beforeunload", () => {
+      this.saveToLocalStorage();
+    });
+  }
+
+  componentWillUnmount() {
+    this.saveToLocalStorage();
+    window.removeEventListener("beforeunload", () => {
+      this.saveToLocalStorage();
+    });
   }
 
   render() {
     return (
-      <div>
+      <div className={styles.search_bar}>
+        <label htmlFor="search_bar">Search:</label>
         <input
+          name="search_bar"
+          className={styles.search_input}
           value={this.state.value}
+          placeholder="Type to search Cards..."
           onInput={(event) => {
             this.handleInput(event);
           }}
