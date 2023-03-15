2385f0adfea1e0b8bdd3a66f154d20e8c848ca83
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-15 22:23:24 +0300

    style: change margin for CardList and SearchBar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import SearchBar from "@/components/Home/SearchBar/SearchBar";
 import CardsList from "@/components/Home/CardsList/CardsList";
-import styles from "./Home.module.scss";
 
 function Home() {
   return (
-    <div className={styles.home}>
+    <>
       <SearchBar />
       <CardsList />
-    </div>
+    </>
   );
 }
 
