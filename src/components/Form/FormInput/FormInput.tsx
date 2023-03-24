0ffb9f8e327abffaae7f0808fe6095b1e4afc7f7
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-24 18:28:00 +0300

    feat: implement FormInput component

---
+++ src/components/Form/FormInput/FormInput.tsx
@@ -0,0 +1,25 @@
+import React, { forwardRef } from "react";
+import styles from "./FormInput.module.scss";
+import FormError from "@/components/Form/FormError/FormError";
+
+interface IProps {
+  type: "text" | "date";
+  ref: React.RefObject<HTMLInputElement>;
+  id: string;
+  label: string;
+  showError: boolean | null;
+  errorMessage: string;
+}
+
+const FormInput: React.ForwardRefExoticComponent<
+  React.PropsWithoutRef<IProps> & React.RefAttributes<HTMLInputElement>
+> = forwardRef(({ type, id, label, showError, errorMessage }, ref) => (
+  <div className={styles.container}>
+    <label>
+      {label} <input type={type} id={id} ref={ref} />
+    </label>
+    {showError && <FormError message={errorMessage} />}
+  </div>
+));
+
+export default FormInput;
