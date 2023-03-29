c4d25444b0659d92784b85a53749f85a233681b3
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-29 23:52:50 +0300

    refactor: apply useLocation hook

--- src/shared/utils/getPageTitle.ts
+++ src/shared/utils/getPageTitle.ts
@@ -1,14 +1,10 @@
-import { IRouterPath } from "@/interfaces/routerPath.interface";
+import { ROUTER_CONFIG } from "@/shared/routerConfig";
 
-export default function getPageTitle(routerConfig: IRouterPath[]) {
-  const { pathname } = location;
-
-  if (routerConfig.some((route) => route.path.includes(pathname))) {
-    const foundElements = routerConfig.filter((route) => route.path === pathname);
-    if (foundElements.length === 1) {
-      const pageName = foundElements.shift()!;
-      return pageName.title;
-    }
+export default function getPageTitle(pathname: string) {
+  const foundElements = ROUTER_CONFIG.filter((route) => route.path === pathname);
+  if (foundElements.length === 1) {
+    const pageName = foundElements.shift()!;
+    return pageName.title;
   }
 
   return "Page not found";
