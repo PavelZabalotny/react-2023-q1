a079f002cae9fc0941988cd4872cc44b3fb2b528
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-03-29 21:43:11 +0300

    refactor: rewrite to stateless function component

--- src/components/Home/CardsList/CardsList.tsx
+++ src/components/Home/CardsList/CardsList.tsx
@@ -1,18 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "./Card/Card";
 import styles from "./CardsList.module.scss";
 import { fakeData } from "@/data/fakeData";
 
-class CardsList extends Component {
-  render() {
-    return (
-      <ul className={styles.cards_list}>
-        {fakeData.map((card) => (
-          <Card key={card.id} props={card} />
-        ))}
-      </ul>
-    );
-  }
-}
+const CardsList = () => (
+  <ul className={styles.cards_list}>
+    {fakeData.map((card) => (
+      <Card key={card.id} props={card} />
+    ))}
+  </ul>
+);
 
 export default CardsList;
