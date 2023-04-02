83edb7ce00f7678f9cbdb5539e8b99cd1c274280
Pavel Zabalotny <pavelzabalotny@gmail.com>
2023-04-02 16:19:25 +0300

    feat: implement CardModal component

--- src/components/Home/CardsList/Card/Card.tsx
+++ src/components/Home/CardsList/Card/Card.tsx
@@ -1,30 +1,44 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "./Card.module.scss";
 
 import { IPeople } from "@/interfaces/people.interface";
+import CardModal from "@/components/Home/CardsList/Card/CardModal/CardModal";
 
 interface IProps {
   card: IPeople;
 }
 
-const Card: FC<IProps> = ({ card: { name, birth_year, gender } }) => {
+const Card: FC<IProps> = ({ card }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  function handleClick(): void {
+    setShowModal(true);
+  }
+
+  function closeModal(): void {
+    setShowModal(false);
+  }
+
   return (
-    <li className={styles.card}>
-      <ul>
-        <li>
-          <span className={styles.bold}>Name:</span>
-          {name}
-        </li>
-        <li>
-          <span className={styles.bold}>Birth year:</span>
-          {birth_year}
-        </li>
-        <li>
-          <span className={styles.bold}>Gender:</span>
-          {gender}
-        </li>
-      </ul>
-    </li>
+    <>
+      <li className={styles.card} onClick={handleClick}>
+        <ul>
+          <li>
+            <span className={styles.bold}>Name:</span>
+            {card.name}
+          </li>
+          <li>
+            <span className={styles.bold}>Birth year:</span>
+            {card.birth_year}
+          </li>
+          <li>
+            <span className={styles.bold}>Gender:</span>
+            {card.gender}
+          </li>
+        </ul>
+      </li>
+      <CardModal card={card} isShow={showModal} onClose={() => closeModal()} />
+    </>
   );
 };
 
